76af8b67202d67da22c0d9d3da04df0abc7f9424
fed1k <allamurotovcoder@mail.ru>
2022-08-04 22:00:40 +0500

    Writing integration tests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_user, only: %i[show edit update destroy]
 
   # GET /users or /users.json
